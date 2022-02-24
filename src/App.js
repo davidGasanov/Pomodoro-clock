dc8c72805160852f8d1888320b75aa1843428f2f
JohnnyVincent <tansacal@protonmail.com>
2022-02-24 12:57:08 +0500

    Formatted strings. Added reset button for pomodoro. Began working on break mode

--- src/App.js
+++ src/App.js
@@ -5,16 +5,29 @@ import TimeDisplay from './components/TimeDisplay';
 
 function App() {
 
-const [initialTime, setInitialTime] = useState(1500)
+const [initialTime, setInitialTime] = useState(3);
+const [breakTime, setBreakTime] = useState(300);
+
 const [pomodoro, setPomodoro] = useState(initialTime); 
 const [counterStatus, setCounterStatus] = useState(false);
 
+// There are 3 mode statuses: pomodoro, break and longBreak. The default is pomodoro.
+const [modeStatus, changeMode] = useState("pomodoro");
 
-// snapshot of time when the counter is initialized/paused (used to avoid the 1 second delay)
+
+// snapshot of time when the counter is initialized/paused (used to avoid the 1 second delay).
 
 const [timeSnapshot, takeSnapshot] = useState(initialTime)
 
 
+useEffect(()=>{
+  
+  if (pomodoro === 0){
+    changeMode("break")
+
+  }  
+},[pomodoro]
+)
 
 
 useEffect (()=>{
@@ -28,28 +41,45 @@ useEffect (()=>{
 
 const toggleCounter = () =>{
   
+  modeStatus
   setCounterStatus(!counterStatus)
   takeSnapshot(pomodoro);
+  
 
 }
 
 
 // Time control 
 
+const addMinute = () =>{
+
+  counterStatus? setInitialTime(pomodoro+60) : setInitialTime(initialTime+60);
+  
+  }
+  
+
 const subtractMinute = () =>{
 
   counterStatus? setInitialTime(pomodoro-60) : setInitialTime(initialTime-60);
-  
-  }
+    
+}
+
+
+const resetTimer = () =>{
+  setCounterStatus(false);
+  setPomodoro(initialTime);
+}
 
 
   return (
     <div className="App">
 
-      <TimeDisplay pomodoro={counterStatus? pomodoro : timeSnapshot} setPomodoro={setPomodoro} counterStatus={counterStatus}/>
+      <TimeDisplay modeStatus={modeStatus} pomodoro={counterStatus? pomodoro : timeSnapshot} setPomodoro={setPomodoro} 
+      breakTime={breakTime} setBreakTime={setBreakTime} counterStatus={counterStatus}/>
       <button onClick={toggleCounter}>Toggle</button>
       <button onClick={addMinute}>Add 1 minute</button>
-
+      <button onClick={subtractMinute}>Subtract 1 minute</button>
+      <button onClick={resetTimer}>Reset</button>
       
     </div>
   );
--- src/components/TimeDisplay.js
+++ src/components/TimeDisplay.js
@@ -1,29 +1,57 @@
 
 import { useEffect } from "react";
 
-const TimeDisplay = ({pomodoro, setPomodoro, counterStatus}) => {
+const TimeDisplay = ({modeStatus, pomodoro, setPomodoro, breakTime, setBreakTime, counterStatus}) => {
+
+  console.log(modeStatus, breakTime);
 
 useEffect(()=>{
 
   if(counterStatus){
 
+    if(modeStatus == "pomodoro"){
       const timeOut = setTimeout(()=>{
       setPomodoro(pomodoro-1) 
-      
   },1000)
+    clearTimeout(timeOut-1);
+} else if( modeStatus == "break"){
 
-  console.log(timeOut);
+  console.log("breakTimeOut fired");
 
-  clearTimeout(timeOut-1);
+  const timeOut = setTimeout(()=>{
+    setBreakTime(breakTime-1) 
+},1000)
+    clearTimeout(timeOut-1);
+}
+
+
 
 }
 
 
-},[pomodoro, counterStatus])
+},[pomodoro, breakTime, counterStatus])
 
+let timeVar
 
-let minutes = Math.floor(pomodoro / 60);
-let seconds = pomodoro % 60 === 0? "00" : pomodoro % 60;
+switch(modeStatus){
+
+  case "pomodoro":
+
+    timeVar = pomodoro;
+    break;
+
+  case "break":
+
+    timeVar = breakTime;
+    break;
+} 
+
+let minutes = Math.floor(timeVar / 60);
+let seconds = timeVar % 60 === 0? "00" : timeVar % 60;
+
+if (seconds < 10){ 
+  seconds = seconds.toString().padStart(2, '0');
+}
 
   return (
     <div className="display-box">{minutes}:{seconds}</div>
